58a624c6aa1e9cccc825357e72e319660a9b645d
Meng <MengSung@gmail.com>
2018-08-20 18:36:17 -0700

    Passed changeVideo and videoList action dispatchers

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,11 @@
-var changeVideo = (video) => ({
+const CHANGE_VIDEO = 'CHANGE_VIDEO';
+
+var changeVideo = (video) => {
   //TODO:  Return some action object to change the currently playing video.
-});
+  return {
+    type: CHANGE_VIDEO,
+    video
+  }
+};
 
 export default changeVideo;
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,11 @@
-var changeVideoList = (videos) => ({
+const CHANGE_VIDEO_LIST = 'CHANGE_VIDEO_LIST';
+
+var changeVideoList = (videos) => {
   //TODO:  Return some action object to change the list of videos in VideoList.
-});
+  return {
+    type: CHANGE_VIDEO_LIST,
+    videos
+  }
+};
 
 export default changeVideoList;
